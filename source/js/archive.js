524d846ec2d7b49094279df0b0b702adeeda5a54
chuguixin <chuguixin@163.com>
2015-09-28 17:17:55 +0800

    update

---
+++ source/js/archive.js
@@ -0,0 +1,9 @@
+$(function () {
+    var $pageMain = $('.archive-main');
+    var $timeTitle = $pageMain.find('.time-title-link');
+    for (var i = 0, l = $timeTitle.length; i < l; i++) {
+        $timeTitle.eq(i).sticky({
+            topSpacing: $('.base-menu').height() + $timeTitle.eq(i).height() * i
+        });
+    }
+})
